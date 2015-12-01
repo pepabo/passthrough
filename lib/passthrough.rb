6e8b2359435c8329ea813c922a71700988050057
Kentaro Kuribayashi <kentarok@gmail.com>
2015-12-01 18:55:21 +0900

    initial commit

--- lib/passthrough.rb
+++ lib/passthrough.rb
@@ -1,5 +1,85 @@
-require "passthrough/version"
+require "net/ssh/gateway"
 
 module Passthrough
-  # Your code goes here...
+  @conns = {}
+  @m     = Mutex.new
+
+  def self.create(name, gateway_host, gateway_user, ssh_options = {})
+    @m.synchronize {
+      @conns[name] = Connection.new(gateway_host, gateway_user, ssh_options)
+    }
+  end
+
+  def self.set(name, key, value)
+    @m.synchronize {
+      conn_of(name).set(key, value)
+    }
+  end
+
+  def self.get(name, key)
+    @m.synchronize {
+      conn_of(name).get(key)
+    }
+  end
+
+  def self.open(name)
+    @m.synchronize {
+      conn_of(name).open
+    }
+  end
+
+  def self.close(name)
+    @m.synchronize {
+      conn_of(name).close
+    }
+  end
+
+  def self.conn_of(name)
+    c = @conns[name]
+    unless c
+      raise ArgumentError, "no such connection: #{name}"
+    end
+    c
+  end
+
+  class Connection
+    def initialize(gateway_host, gateway_user, ssh_options)
+      @gateway_host  = gateway_host
+      @gateway_user  = gateway_user
+      @ssh_options   = ssh_options
+      @params        = {}
+
+      @original_host
+      @original_port
+    end
+
+    def set(key, value)
+      @params[key] = value
+    end
+
+    def get(key)
+      @params[key]
+    end
+
+    def open
+      @conn = Net::SSH::Gateway.new(@gateway_host, @gateway_user, @ssh_options)
+      port = @conn.open(@params[:host], @params[:port])
+
+      # Replace the destination with local gateway
+      @original_host = @params[:host]
+      @params[:host] = '127.0.0.1'
+      @original_port = @params[:port]
+      @params[:port] = port
+    end
+
+    def close
+      # Recover original destination
+      @params[:host] = @original_host
+      @original_host = nil
+      @params[:port] = @original_port
+      @original_port = nil
+
+      @conn.shutdown!
+    end
+  end
 end
--- passthrough.gemspec
+++ passthrough.gemspec
@@ -6,26 +6,20 @@ require 'passthrough/version'
 Gem::Specification.new do |spec|
   spec.name          = "passthrough"
   spec.version       = Passthrough::VERSION
-  spec.authors       = ["Kentaro Kuribayashi"]
-  spec.email         = ["kentarok@gmail.com"]
+  spec.authors       = ["Takatoshi Ono", "Kentaro Kuribayashi"]
+  spec.email         = ["takatoshi.ono@gmail.com", "kentarok@gmail.com"]
 
-  spec.summary       = %q{TODO: Write a short summary, because Rubygems requires one.}
-  spec.description   = %q{TODO: Write a longer description or delete this line.}
-  spec.homepage      = "TODO: Put your gem's website or public repo URL here."
+  spec.summary       = %q{Provides pass-through functionality using net-ssh-gateway.}
+  spec.description   = %q{Provides pass-through functionality using net-ssh-gateway.}
+  spec.homepage      = "https://github.com/pepabo/passthrough"
   spec.license       = "MIT"
-
-  # Prevent pushing this gem to RubyGems.org by setting 'allowed_push_host', or
-  # delete this section to allow pushing this gem to any host.
-  if spec.respond_to?(:metadata)
-    spec.metadata['allowed_push_host'] = "TODO: Set to 'http://mygemserver.com'"
-  else
-    raise "RubyGems 2.0 or newer is required to protect against public gem pushes."
-  end
 
   spec.files         = `git ls-files -z`.split("\x0").reject { |f| f.match(%r{^(test|spec|features)/}) }
   spec.bindir        = "exe"
   spec.executables   = spec.files.grep(%r{^exe/}) { |f| File.basename(f) }
   spec.require_paths = ["lib"]
+
+  spec.add_dependency "net-ssh-gateway"
 
   spec.add_development_dependency "bundler", "~> 1.10"
   spec.add_development_dependency "rake", "~> 10.0"
